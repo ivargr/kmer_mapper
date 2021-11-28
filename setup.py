e2082e8d07e5ad5c5859d286f16373c0df701ef7
Ivar Grytten <ivar.grytten@gmail.com>
2021-11-28 16:54:34 +0100

    Fixed dependencies

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy as np
 
 ext_modules=[
     Extension("kmer_mapper.mapper",
               ["kmer_mapper/mapper.pyx"],
               libraries=["m"],
-              extra_compile_args = ["-O3", "-ffast-math", "-march=native", "-fopenmp" ],
-              extra_link_args=['-fopenmp']
+              extra_compile_args = ["-O3", "-march=native"],
               )
       ]
 
 setup(name='kmer_mapper',
-      version='0.0.1',
+      version='0.0.3',
       description='Kmer Mapper',
       url='http://github.com/ivargr/kmer_mapper',
       author='Ivar Grytten',
@@ -22,7 +22,7 @@ setup(name='kmer_mapper',
       license='MIT',
       packages=["kmer_mapper"],
       zip_safe=False,
-      install_requires=['numpy', 'cython', 'graph_kmer_index'],
+      install_requires=['numpy==1.20.3', 'cython', 'graph_kmer_index>=0.0.3', 'pathos', 'scipy', 'pandas'],
       classifiers=[
             'Programming Language :: Python :: 3'
       ],
@@ -30,5 +30,16 @@ setup(name='kmer_mapper',
             'console_scripts': ['kmer_mapper=kmer_mapper.command_line_interface:main']
       },
       cmdclass = {"build_ext": build_ext},
-      ext_modules = ext_modules
+      #ext_modules = ext_modules
+      include_dirs=np.get_include(),
+      ext_modules=cythonize(["kmer_mapper/mapper.pyx"]),
 )
+
+"""
+rm -rf dist
+python3 setup.py sdist bdist_wheel
+auditwheel repair --plat manylinux_2_17_x86_64 dist/kmer_mapper-*-cp38-cp38-linux_x86_64.whl
+rm dist/*.whl
+mv wheelhouse/* dist
+python3 -m twine upload --repository pypi dist/*
+"""
