051125b08899f22da6c2fed90ba02577497518b1
Ivar Grytten <ivar.grytten@gmail.com>
2021-12-08 14:59:39 +0100

    test comparing old and new mapping

---
+++ tests/test_kmer_lookup_old_and_new.py
@@ -0,0 +1,24 @@
+from kmer_mapper.kmer_lookup import Advanced2
+from graph_kmer_index import KmerIndex
+import numpy as np
+np.random.seed(1)
+
+def test():
+    old_index = KmerIndex.from_file("kmer_index_only_variants.npz")
+    new_index = Advanced2.from_old_index_files("kmer_index_only_variants.npz")
+
+    random_kmers = np.random.randint(0, 4**31, 1000)
+    kmers = old_index._kmers
+
+
+    for kmer in kmers:
+        old_hits = old_index.get(int(kmer), max_hits=1000000000)[0]
+        kmer = np.array([kmer], dtype=np.uint64)
+        new_hits = np.nonzero(new_index.get_node_counts(kmer))[0]
+
+        print(kmer, old_hits, new_hits)
+
+        assert set(old_hits) == set(new_hits), "Fail for kmer %d" % kmer
+
+
+test()
