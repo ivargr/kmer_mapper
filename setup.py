aba9754cfedc1386b0ed0c77640511080e155ce2
Ivar Grytten <ivar.grytten@gmail.com>
2021-12-13 11:47:06 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ ext_modules=[
 """
 
 setup(name='kmer_mapper',
-      version='0.0.9',
+      version='0.0.10',
       description='Kmer Mapper',
       url='http://github.com/ivargr/kmer_mapper',
       author='Ivar Grytten',
@@ -24,7 +24,7 @@ setup(name='kmer_mapper',
       license='MIT',
       packages=["kmer_mapper"],
       zip_safe=False,
-      install_requires=['numpy', 'cython', 'graph_kmer_index', 'pathos', 'scipy', 'pandas', 'shared_memory_wrapper'],
+      install_requires=['numpy', 'cython', 'graph_kmer_index>=0.0.10', 'pathos', 'scipy', 'pandas', 'shared_memory_wrapper'],
       classifiers=[
             'Programming Language :: Python :: 3'
       ],
