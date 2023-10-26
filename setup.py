ff3cb7c523132f1f14d1413f31a26b0d3ff129ab
Ivar Grytten <ivar.grytten@gmail.com>
2023-10-26 15:49:53 +0200

    version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ ext_modules=[
       ]
 
 setup(name='kmer_mapper',
-      version='0.0.35',
+      version='0.0.36',
       description='Kmer Mapper',
       url='http://github.com/ivargr/kmer_mapper',
       author='Ivar Grytten',
